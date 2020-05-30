6ba05fa86062ab2b33830fa14dd72744695c85ef
nn <rohan.rawat.9897@gmail.com>
2020-05-30 13:05:59 +0530

    1st

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native';
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  } 
+
+  render() {
+    return (
+      <View>
+        <Text> hello home..baka </Text>
+      </View>
+    )
+  }
+}
+
+export default App;
